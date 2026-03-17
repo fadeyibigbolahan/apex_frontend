e487a90c4c6a7d90007a4411afd8b4cff796ae8d
fadeyibi gbolahan <fadeyibi26@gmail.com>
2026-03-17 15:59:51 +0100

    wwoo0

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/apex_frontend/", // Ensure correct base path
   plugins: [react()],
+  base: "/apex_frontend/", // Add this line - must match your repo name exactly
 });
