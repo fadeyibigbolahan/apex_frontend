a8607df6486cc0e7ece7b9cbebf76386f234e632
fadeyibi gbolahan <fadeyibi26@gmail.com>
2026-03-18 10:08:50 +0100

    Updates

---
+++ assets/chevron-left-C2nLjgce.js
@@ -0,0 +1,11 @@
+import{d as e}from"./index-CzRleNsx.js";/**
+ * @license lucide-react v0.441.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("Ban",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m4.9 4.9 14.2 14.2",key:"1m5liu"}]]);/**
+ * @license lucide-react v0.441.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{c as B,t as C};
